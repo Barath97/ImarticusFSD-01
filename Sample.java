18743fc59ee620f61363876de270cfcff429e4d2
Barath <barath.kj97@gmail.com>
2023-06-21 13:01:09 +0530

    Changed

--- Sample.java
+++ Sample.java
@@ -1,6 +1,5 @@
 Class First{
     Public static void main(String[] args){
-        System.out.println("Git Commited");
-        System.out.println("code changed");
+        System.out.println("Code changed in file");
     }
 }
